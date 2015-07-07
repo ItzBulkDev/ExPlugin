50660b03c7428bfcad9cff1cd353a55f53d09318
Bulk Developer <jaydenpatrick1110@gmail.com>
2015-07-07 18:55:15 -0400

    Update Main.php

--- src/ItzBulkDev/Main.php
+++ src/ItzBulkDev/Main.php
@@ -1,20 +1,16 @@
 <?php
 
-namespace exPlugin;
+namespace ;
 
-use pocketmine\Player;
-use pocketmine\event\player\PlayerJoinEvent
-use pocketmine\utils\TextFormat
-use pocketmine\plugin\PluginBase;
-use pocketmine\event\Listener;
+use pocketmine\;
 
 class Main extends PluginBase implements Listener{
 
 public function onEnable(){
-  $this->getLogger()->info(TextFormat::GREEN."[exPlugin] Enabled!");
+  $this->getLogger()->info(TextFormat::GREEN." Enabled!");
   $p = $event->getPlayer()->getName();
 }
-public function onPlayerJoin(PlayerJoinEvent $event){
-  $p->sendPopup("§4Welcome §5To §6The §7Server §8" . $p . "!");
-}
+
+
+
 }
